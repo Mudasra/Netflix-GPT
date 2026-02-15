1f9f89a05f0148101b466697565a4b1decd890f1
Mudasra <km4446245@gmail.com>
2026-02-15 15:54:49 +0500

    Main Container

--- src/components/VideoBg.js
+++ src/components/VideoBg.js
@@ -11,7 +11,7 @@ const VideoBg = ({movieId}) => {
     <div className="">
      {trailerVideo && (
   <iframe
-    className="w-screen aspect-video"
+    className="aspect-video"
     src={`https://www.youtube.com/embed/${trailerVideo.key}?autoplay=1&mute=1&controls=0&loop=1&rel=0&playlist=${trailerVideo.key}`}
     title="YouTube video player"
     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
